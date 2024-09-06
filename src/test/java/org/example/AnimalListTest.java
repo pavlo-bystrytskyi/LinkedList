23ccf3d97eeeb5d03e639232f34b3349da34cc4c
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-09-06 21:04:13 +0200

    Tests added.

---
+++ src/test/java/org/example/AnimalListTest.java
@@ -0,0 +1,136 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalListTest {
+
+    @org.junit.jupiter.api.Test
+    void addTest_addNoAnimals() {
+        AnimalList animalList = new AnimalList();
+
+        String expected = "";
+        String actual = animalList.toString();
+
+        Assertions.assertEquals(
+              expected,
+              actual
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void addTest_addMultipleAnimals() {
+        Animal cow = new Animal("Cow");
+        Animal cat = new Animal("Cat");
+        Animal dog = new Animal("Dog");
+        Animal pig = new Animal("Pig");
+        Animal firstSheep = new Animal("Sheep");
+        Animal secondSheep = new Animal("Sheep");
+        AnimalList animalList = new AnimalList();
+
+        animalList.add(cow);
+        animalList.add(cat);
+        animalList.add(dog);
+        animalList.add(pig);
+        animalList.add(firstSheep);
+        animalList.add(secondSheep);
+        animalList.add(pig);
+
+        String expected = "Cow -> Cat -> Dog -> Pig -> Sheep -> Sheep -> Pig";
+        String actual = animalList.toString();
+
+        Assertions.assertEquals(
+              expected,
+              actual
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeTest_removeFromBeginning() {
+        Animal cow = new Animal("Cow");
+        Animal cat = new Animal("Cat");
+        Animal dog = new Animal("Dog");
+        Animal pig = new Animal("Pig");
+        Animal firstSheep = new Animal("Sheep");
+        Animal secondSheep = new Animal("Sheep");
+        AnimalList animalList = new AnimalList();
+
+        animalList.add(cow);
+        animalList.add(cat);
+        animalList.add(dog);
+        animalList.add(pig);
+        animalList.add(firstSheep);
+        animalList.add(secondSheep);
+        animalList.add(pig);
+
+        animalList.remove(cow);
+
+        String expected = "Cat -> Dog -> Pig -> Sheep -> Sheep -> Pig";
+        String actual = animalList.toString();
+
+        Assertions.assertEquals(
+                expected,
+                actual
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeTest_removeFromEnd() {
+        Animal cow = new Animal("Cow");
+        Animal cat = new Animal("Cat");
+        Animal dog = new Animal("Dog");
+        Animal pig = new Animal("Pig");
+        Animal firstSheep = new Animal("Sheep");
+        Animal secondSheep = new Animal("Sheep");
+        AnimalList animalList = new AnimalList();
+
+        animalList.add(cow);
+        animalList.add(cat);
+        animalList.add(pig);
+        animalList.add(firstSheep);
+        animalList.add(secondSheep);
+        animalList.add(pig);
+        animalList.add(dog);
+
+        animalList.remove(dog);
+
+        String expected =  "Cow -> Cat -> Pig -> Sheep -> Sheep -> Pig";
+        String actual = animalList.toString();
+
+        Assertions.assertEquals(
+                expected,
+                actual
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeTest_removeMultiple() {
+        Animal cow = new Animal("Cow");
+        Animal cat = new Animal("Cat");
+        Animal dog = new Animal("Dog");
+        Animal pig = new Animal("Pig");
+        Animal firstSheep = new Animal("Sheep");
+        Animal secondSheep = new Animal("Sheep");
+        AnimalList animalList = new AnimalList();
+
+        animalList.add(cow);
+        animalList.add(cat);
+        animalList.add(pig);
+        animalList.add(firstSheep);
+        animalList.add(secondSheep);
+        animalList.add(pig);
+        animalList.add(dog);
+
+        animalList.remove(firstSheep);
+        animalList.remove(pig);
+
+        String expected =  "Cow -> Cat -> Dog";
+        String actual = animalList.toString();
+
+        Assertions.assertEquals(
+                expected,
+                actual
+        );
+    }
+}
